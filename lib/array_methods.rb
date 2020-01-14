5a0dabb8a7b7d3579adbc98a477594b4ca5eacba
Robert Collins <rpc219@gmail.com>
2020-01-14 22:12:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,10 @@
 def find_element_index(array, value_to_find)
-  count = 0
-  while array[count] do
+  array.length.times do |count|
     if array[count] == value_to_find
       return count
     end
-    count += 1
   end
+  nil
 end
 
 def find_max_value(array)
