73b9ce05ab9c06d82f7a18f8dcbe5fa6de15b000
Robert Collins <rpc219@gmail.com>
2020-01-14 22:15:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,17 +14,15 @@ def find_max_value(array)
       max = array[count]
     end
   end
-  return max
+  max
 end
 
 def find_min_value(array)
-  array_with_min = [array[0]]
-  count = 0
-  while array[count] do
-    if array[count] < array_with_min.last
-      array_with_min << array[count]
+  min = 0
+  array.length.times do |count|
+    if array[count] > min
+      min = array[count]
     end
-  count += 1
   end
-  return array_with_min.last
+  min
 end
